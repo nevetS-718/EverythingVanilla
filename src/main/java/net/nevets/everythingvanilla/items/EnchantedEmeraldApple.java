5d8cba9e80901427d9b1e68b7d94d64c918adc82
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 11:50:23 -0500

    Create EnchantedEmeraldApple.java
    added enchanted emerald apple class

---
+++ src/main/java/net/nevets/everythingvanilla/items/EnchantedEmeraldApple.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.items;
+
+import net.mineceraft.item.Item;
+
+public class EnchantedEmeraldApple extends Item {
+
+  public EnchantedEmeraldApple(Settings settings) {
+  
+    super(settings);
+  
+  }
+  
+}
