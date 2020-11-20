2739e86e76fe6dbbd3002a00327d594f838fe876
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 11:53:14 -0500

    Create PlatinumApple.java
    added class for platinum apple

---
+++ src/main/java/net/nevets/everythingvanilla/items/PlatinumApple.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.items;
+
+import net.minecraft.item.Item;
+
+public class PlatinumApple extends Item {
+
+  public PlatinumApple(Settings settings) {
+  
+    super(settings);
+    
+  }
+  
+}
