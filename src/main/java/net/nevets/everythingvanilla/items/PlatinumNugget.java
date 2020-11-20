bbe574dd071bfa0ba9e23dc516f99df4b781373b
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 08:51:54 -0500

    Add files via upload

---
+++ src/main/java/net/nevets/everythingvanilla/items/PlatinumNugget.java
@@ -0,0 +1,9 @@
+package net.nevets.everythingvanilla.items;
+
+import net.minecraft.item.Item;
+
+public class PlatinumNugget extends Item {
+    public PlatinumNugget(Settings settings) {
+        super(settings);
+    }
+}
