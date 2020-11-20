51b234e06fe10e6ab32b43d16328898da2237d8b
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 09:53:14 -0500

    Create EnchantedIronApple.java

---
+++ src/main/java/net/nevets/everythingvanilla/items/EnchantedIronApple.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.items;
+
+import net.minecraft.item.Item;
+
+public class EnchantedIronApple extends Item {
+
+  public EnchantedIronApple(Settings item$Settings_1) { 
+
+    super(item$Settings_1); 
+  
+  }
+
+}
