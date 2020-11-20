fe96de2c8a9fe2ff9e34f364440f9610cdfc8e27
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 09:31:47 -0500

    Create IronApple.java

---
+++ src/main/java/net/nevets/everythingvanilla/items/IronApple.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.items;
+
+import net.minecraft.item.Item;
+
+public class IronApple extends Item {
+
+  public CookedVillagerMeat(Settings item$Settings_1) { 
+
+    super(item$Settings_1); 
+  
+  }
+
+}
