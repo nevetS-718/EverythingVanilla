3a50434be343283249e51c243968e861dd72a8cf
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 11:45:44 -0500

    Create EnchantedDiamondApple.java
    added class for enchented diamond apple

---
+++ src/main/java/net/nevets/everythingvanilla/items/EnchantedDiamondApple.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.items;
+
+import net.minecraft.item;
+
+public class EnchantedDiamondApple extends Item {
+
+  public EnchantedDiamondApple(Settings settings) {
+  
+    super(settings);
+  
+  }
+
+}
