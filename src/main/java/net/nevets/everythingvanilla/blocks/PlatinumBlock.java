a3e78aae6027deb840746d6b05cfd4546a791e4e
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 08:51:21 -0500

    Add files via upload

---
+++ src/main/java/net/nevets/everythingvanilla/blocks/PlatinumBlock.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.blocks;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+
+public class PlatinumBlock extends Block {
+    public PlatinumBlock() {
+        super(FabricBlockSettings.of(Material.STONE).breakByHand(false).breakByTool(FabricToolTags.PICKAXES).sounds(BlockSoundGroup.METAL).strength(30, 1000f));
+    }
+}
