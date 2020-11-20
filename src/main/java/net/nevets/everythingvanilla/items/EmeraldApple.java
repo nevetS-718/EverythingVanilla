648fb6239bc810876a3333590c0fc6ab9a9f0722
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 11:47:50 -0500

    Create EmeraldApple.java
    added class for emerald apple

---
+++ src/main/java/net/nevets/everythingvanilla/items/EmeraldApple.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.items;
+
+import net.minecraft.item.Item;
+
+public class EmeraldApple extends Item {
+
+  public EmeraldApple(Settings settings) {
+  
+    super(settings);
+  
+  }
+
+}
