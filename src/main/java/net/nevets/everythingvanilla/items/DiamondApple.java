dcdc064fd3ddeabcc6ad7481e0ce7909dec6736a
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-20 09:57:35 -0500

    Create DiamondApple.java
    added diamond apple class

---
+++ src/main/java/net/nevets/everythingvanilla/items/DiamondApple.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.items;
+
+import net.minecraft.item.Item;
+
+public class DiamondApple extends Item {
+
+  public DiamondApple(Settings item$Settings_1) { 
+
+    super(item$Settings_1); 
+  
+  }
+
+}
