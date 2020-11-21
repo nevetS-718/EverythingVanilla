f9312cff7b5148c3dc7177e703877539b9edd789
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-21 00:49:24 -0500

    Update AxeBase.java

--- src/main/java/net/nevets/everythingvanilla/tools/AxeBase.java
+++ src/main/java/net/nevets/everythingvanilla/tools/AxeBase.java
@@ -6,6 +6,6 @@ import net.nevets.everythingvanilla.Main;
 
 public class AxeBase extends AxeItem {
     public AxeBase(ToolMaterial toolMaterial_1) {
-        super(toolMaterial_1, 5, -3.0f, new Settings().group(Main.ALL));
+        super(toolMaterial_1, 5, -3.0f, new Settings().group(Main.TOOLS));
     }
 }
