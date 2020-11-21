fb657ab182ffacbabcc2580973374aabdbfb014c
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-21 00:50:02 -0500

    Update HoeBase.java

--- src/main/java/net/nevets/everythingvanilla/tools/HoeBase.java
+++ src/main/java/net/nevets/everythingvanilla/tools/HoeBase.java
@@ -6,6 +6,6 @@ import net.nevets.everythingvanilla.Main;
 
 public class HoeBase extends HoeItem {
     public HoeBase(ToolMaterial toolMaterial_1) {
-        super(toolMaterial_1, -3, 0f, new Settings().group(Main.ALL));
+        super(toolMaterial_1, -3, 0f, new Settings().group(Main.TOOLS));
     }
 }
