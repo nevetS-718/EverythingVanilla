92bdb62017a659d329f2338ec8f0d6837422a213
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-21 00:50:53 -0500

    Update ShovelBase.java

--- src/main/java/net/nevets/everythingvanilla/tools/ShovelBase.java
+++ src/main/java/net/nevets/everythingvanilla/tools/ShovelBase.java
@@ -6,6 +6,6 @@ import net.nevets.everythingvanilla.Main;
 
 public class ShovelBase extends ShovelItem {
     public ShovelBase(ToolMaterial toolMaterial_1) {
-        super(toolMaterial_1, 1, -3f, new Settings().group(Main.ALL));
+        super(toolMaterial_1, 1, -3f, new Settings().group(Main.TOOLS));
     }
 }
