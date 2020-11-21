d3eeb9c03974e6ffb5336313c948f3bb5727f3cd
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-21 00:50:17 -0500

    Update PickaxeBase.java

--- src/main/java/net/nevets/everythingvanilla/tools/PickaxeBase.java
+++ src/main/java/net/nevets/everythingvanilla/tools/PickaxeBase.java
@@ -7,6 +7,6 @@ import net.nevets.everythingvanilla.Main;
 
 public class PickaxeBase extends PickaxeItem {
     public PickaxeBase(ToolMaterial toolMaterial_1) {
-        super(toolMaterial_1, 1, -2.8f, new Item.Settings().group(Main.ALL));
+        super(toolMaterial_1, 1, -2.8f, new Item.Settings().group(Main.TOOLS));
     }
 }
