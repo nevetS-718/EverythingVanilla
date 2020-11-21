19cba82a6d22db04c10fc6785c9eb0ad204c2054
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-21 00:47:02 -0500

    Update EnchantedEmeraldApple.java

--- src/main/java/net/nevets/everythingvanilla/items/EnchantedEmeraldApple.java
+++ src/main/java/net/nevets/everythingvanilla/items/EnchantedEmeraldApple.java
@@ -4,10 +4,10 @@ import net.minecraft.item.Item;
 
 public class EnchantedEmeraldApple extends Item {
 
-  public EnchantedEmeraldApple(Settings settings) {
-  
-    super(settings);
-  
-  }
-  
+    public EnchantedEmeraldApple(Settings settings) {
+
+        super(settings);
+
+    }
+
 }
