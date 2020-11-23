06a6348d97e6532f3c70ad68d89b1fc9164506cb
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-23 11:01:41 -0500

    Create NetheriteApple.java
    added netherite apple class in the correct location lol

---
+++ src/main/java/net/nevets/everythingvanilla/items/NetheriteApple.java
@@ -0,0 +1,13 @@
+package net.nevets.everythingvanilla.items;
+
+import net.minecraft.item.Item;
+
+public class NetheriteApple extends Item {
+
+  public NetheriteApple(Settings settings) {
+  
+    super(settings);
+  
+  }
+
+}
