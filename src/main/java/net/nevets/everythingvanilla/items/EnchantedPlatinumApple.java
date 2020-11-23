a239d49ebe7ebf955f7047d82da40c0374b47f84
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-11-23 10:50:22 -0500

    Update EnchantedPlatinumApple.java
    Added enchanted glint

--- src/main/java/net/nevets/everythingvanilla/items/EnchantedPlatinumApple.java
+++ src/main/java/net/nevets/everythingvanilla/items/EnchantedPlatinumApple.java
@@ -5,9 +5,10 @@ import net.minecraft.item.Item;
 public class EnchantedPlatinumApple extends Item {
 
     public EnchantedPlatinumApple(Settings settings) {
-
         super(settings);
-
     }
-
+    public boolean hasGlint(ItemStack stack) {
+        return true;
+    }
+    
 }
